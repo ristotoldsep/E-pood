142be48066e0276858add065accea39329a980f3
Risto Tõldsep <r.toldsep@gmail.com>
2021-04-19 23:02:44 +0300

    Muutsin lehe pealkirju

--- about.php
+++ about.php
@@ -29,7 +29,7 @@ if (isset($_SESSION['email'])) {
 <html lang="en">
 
 <head>
-	<title>Ostukorv</title>
+	<title>Meist</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!--===============================================================================================-->
