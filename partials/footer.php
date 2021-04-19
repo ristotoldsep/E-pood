d860ee07bb7ff978787ed602b56553f28bb55ea2
Risto Tõldsep <r.toldsep@gmail.com>
2021-04-19 22:51:11 +0300

    Muutsin footeri sisu

--- partials/footer.php
+++ partials/footer.php
@@ -115,15 +115,9 @@
             </div>
 
             <p class="stext-107 cl6 txt-center">
-                <!-- E-pood. Autoriõigus &copy;<script>
+                E-pood. Autoriõigus &copy;<script>
                     document.write(new Date().getFullYear());
-                </script>. Kõik õigused kaitstud. -->
-                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                Copyright &copy;<script>
-                    document.write(new Date().getFullYear());
-                </script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-
+                </script>. Kõik õigused kaitstud.
             </p>
         </div>
     </div>
